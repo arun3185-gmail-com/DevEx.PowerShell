4ce6a566c72bf76b638747354d85b89f9a7764bf
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2018-10-13 03:08:58 +0530

    Add files via upload

---
+++ ModuleTest_ExcelFile.ps1
@@ -0,0 +1,19 @@
+﻿
+
+
+
+$excelDoc = Create-ExcelFile -FilePath "F:\Arun\Git\DevEx.Data\OpenXmlSheet.xlsx"
+$wksPart = Add-Sheet -XlDoc $excelDoc -SheetName "Sheet0"
+
+$txt = New-Object DocumentFormat.OpenXml.Spreadsheet.Text
+$txt.Text = "Test"
+$ins = New-Object DocumentFormat.OpenXml.Spreadsheet.InlineString
+$ins.AppendChild($txt)
+
+
+$r = Get-Row -Ws $wksPart.Worksheet -RowIndex 1
+$c = Get-Cell -Row $r -ColumnName "A"
+$c.DataType = [DocumentFormat.OpenXml.Spreadsheet.CellValues]::InlineString
+$c.AppendChild($ins)
+
+Save-ExcelFile -XlDoc $excelDoc
