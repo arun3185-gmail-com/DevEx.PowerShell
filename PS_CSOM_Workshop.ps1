d8433eecfc877564cf796d48f87894549f481e33
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2018-11-13 21:52:34 +0530

    Add files via upload

---
+++ PS_CSOM_Workshop.ps1
@@ -0,0 +1,33 @@
+﻿################################################################################################################################################################
+# CSOM Workshop
+################################################################################################################################################################
+Import-Module "D:\Arun\Git\DevEx.References\NuGet\microsoft.sharepointonline.csom.16.1.7414.1200\lib\net45\Microsoft.SharePoint.Client.dll"
+Import-Module "D:\Arun\Git\DevEx.References\NuGet\microsoft.sharepointonline.csom.16.1.7414.1200\lib\net45\Microsoft.SharePoint.Client.Runtime.dll"
+################################################################################################################################################################
+[string] $Username = "Arunkumar.Balasubramani@dxit181001.onmicrosoft.com"
+if ($Password -eq $null) { [SecureString] $Password = Read-Host "Enter Password for $($Username)" -AsSecureString }
+[string] $SiteUrl   = "https://dxit181001.sharepoint.com"
+[string] $ListTitle = "DemoList1"
+
+################################################################################################################################################################
+$context = New-Object Microsoft.SharePoint.Client.ClientContext($SiteUrl)
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username , $Password)
+$context.Credentials = $credentials    
+    
+
+$web = $context.Web
+$context.Load($web)
+$list = $web.Lists.GetByTitle($ListTitle)
+
+$context.ExecuteQuery()
+
+
+
+
+
+
+
+
+
+
+
