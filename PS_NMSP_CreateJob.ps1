5e80523454931f0a0d357b8fc39af05b739de8e7
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2018-11-14 23:26:16 +0530

    Add files via upload

---
+++ PS_NMSP_CreateJob.ps1
@@ -0,0 +1,183 @@
+﻿
+################################################################################################################################################################
+# NMSP Create job
+################################################################################################################################################################
+
+Add-Type -Path "J:\Arun\Git\DevEx.VB.Net\LN.vb"
+
+################################################################################################################################################################
+
+[string] $ServerName = "EMEAARFN01/Server/Evonik"
+[string] $LNFilePath = "Abteilungen/PKM-R/archiv-pkmrabre.nsf"
+
+[string] $jobFileFullPath = "J:\QuestJobs\Template0.pmjob"
+[string] $NewJobFileFullPath = "J:\QuestJobs\Template1.pmjob"
+
+[xml] $jobXmlDoc = $null
+
+[string] $formNamesDelimited = "doku;doku_1;doku_2;doku_3;doku_4"
+
+[string] $webUrl = "https://evonik.sharepoint.com/sites/10426"
+[string] $listTitle = "Archiv"
+[string] $defaultUserName = "Nadine.Jost@evonik.com"
+
+################################################################################################################################################################
+
+function New-SourceDefinitionColumn()
+{
+    Param
+    (
+        [string] $FieldName,
+        [int]    $FieldTypeNumber,
+        [string] $FieldTypeName = ""
+    )
+
+    $Local:SrcDefCol = New-Object PSObject
+    $Local:SrcDefCol | Add-Member NoteProperty -Name "FieldName" -Value $FieldName
+    $Local:SrcDefCol | Add-Member NoteProperty -Name "FieldTypeNumber" -Value $FieldTypeNumber
+    $Local:SrcDefCol | Add-Member NoteProperty -Name "FieldTypeName" -Value $FieldTypeName
+
+    Return $Local:SrcDefCol
+}
+
+try
+{
+    [LN.NotesSession]  $nSession  = New-Object LN.NotesSession
+    [LN.NotesDatabase] $nDatabase = $nSession.GetDatabase($ServerName, $LNFilePath)
+    [LN.NotesDocumentCollection] $docCollection = $nDatabase.AllDocuments
+    
+    
+    
+    $jobXmlDoc = New-Object xml
+    $jobXmlDoc.Load($jobFileFullPath)
+
+    [string[]] $serverNameParts = $ServerName.Split('/')
+    if ($serverNameParts.Count -eq 3)
+    {
+        [System.Xml.XmlNode] $connStrNode = $jobXmlDoc.SelectSingleNode("/TransferJob/QuerySource/ConnectionString")
+        $connStrNode.InnerXml = "server='CN=$($serverNameParts[0])/OU=$($serverNameParts[1])/O=$($serverNameParts[2])'; database='$($LNFilePath)'; zone=utc"
+
+        [System.Xml.XmlNode] $srcDefNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SourceDefinition")
+        $srcDefNode.Attributes["Name"].InnerText = $nDatabase.Title
+        $srcDefNode.Attributes["Templates"].InnerText = $nDatabase.DesignTemplateName
+
+        [System.Xml.XmlNode] $replicaIDNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SourceDefinition/QuerySpec/ReplicaId")
+        $replicaIDNode.InnerText = $nDatabase.ReplicaID
+
+        [PSCustomObject[]] $srcDefColumns = @()
+        [string[]] $arrFormNames = $formNamesDelimited.Split(';')
+        [int[]] $arrFormIndices = @()
+
+        for ($i = 0; $i -lt $nDatabase.Forms.Length; $i++)
+        {
+            if ($nDatabase.Forms[$i].Name -in $arrFormNames) { $arrFormIndices += $i }
+        }
+        
+        foreach ($formIdx in $arrFormIndices)
+        {
+            foreach ($fieldName in $nDatabase.Forms[$formIdx].Fields)
+            {
+                if ($srcDefColumns.Where({ $PSItem.FieldName -eq $fieldName }).Count -eq 0)
+                {
+                    $srcDefColumn = New-SourceDefinitionColumn -FieldName $fieldName -FieldTypeNumber ($nDatabase.Forms[$formIdx].GetFieldType($fieldName))
+                    $srcDefColumns += $srcDefColumn
+                }
+            }
+        }
+
+        [LN.NotesDocument] $doc = $docCollection.GetFirstDocument()
+        while ($doc -ne $null)
+        {
+            if ($doc.GetFirstItem("Form").Text -in $arrFormNames)
+            {
+                for ($i = 0; $i -lt $doc.Items.Length; $i++)
+                {
+                    [LN.NotesItem] $itm = $doc.Items[$i]
+                    $srcDefColumns.Where({ $PSItem.FieldTypeNumber -eq 0 -and $PSItem.FieldName -eq $itm.Name }).ForEach({ $PSItem.FieldTypeNumber = $itm.Type; $PSItem.FieldTypeName = [System.Enum]::GetName([LN.NotesItemDataType], $itm.Type) })
+                }
+            }
+
+            if ($srcDefColumns.Where({ $PSItem.FieldTypeNumber -eq 0 }).Count -eq 0) { exit }
+            
+            $doc = $docCollection.GetNextDocument($doc)
+        }
+
+        if ($srcDefColumns.Where({ [string]::IsNullOrWhiteSpace($PSItem.FieldTypeName) }).Count -gt 0)
+        {
+            $srcDefColumns.Where({ [string]::IsNullOrWhiteSpace($PSItem.FieldTypeName) }).ForEach({ $PSItem.FieldTypeName = [System.Enum]::GetName([LN.NotesItemDataType], $PSItem.FieldTypeNumber) })
+        }
+
+        [System.Xml.XmlNode] $querySpecNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SourceDefinition/QuerySpec")
+        [System.Xml.XmlNode] $unidNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SourceDefinition/QuerySpec/UNID")
+
+        foreach ($srcDefColumn in $srcDefColumns)
+        {
+            [System.Xml.XmlElement] $xmlElmtColumn = $jobXmlDoc.CreateElement("Column")
+
+            $xmlElmtColumn.SetAttribute("ColumnType", "Item")
+            $xmlElmtColumn.SetAttribute("Value", $srcDefColumn.FieldName)
+            
+            if ($srcDefColumn.FieldTypeNumber -in @(1280,1281))
+            {
+                $xmlElmtColumn.SetAttribute("ReturnType", "String")
+            }
+            elseif ($srcDefColumn.FieldTypeNumber -in @(1024))
+            {
+                $xmlElmtColumn.SetAttribute("ReturnType", "Date")
+            }
+            elseif ($srcDefColumn.FieldTypeNumber -in @(1076))
+            {
+                $xmlElmtColumn.SetAttribute("ReturnType", "String")
+                $xmlElmtColumn.SetAttribute("Option", "Multi")
+            }
+            else
+            {
+                $xmlElmtColumn.SetAttribute("ReturnType", "String")
+            }
+
+            $querySpecNode.InsertAfter($xmlElmtColumn, $unidNode)
+            
+            Write-Host "$($srcDefColumn.FieldTypeName) - $($srcDefColumn.FieldName)"
+        }
+
+        [System.Xml.XmlNode] $formsNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SourceDefinition/QuerySpec/Forms")
+        $formsNode.InnerText = $formNamesDelimited
+
+        [System.Xml.XmlNode] $sp_WebNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SharePointConnection/Web")
+        $sp_WebNode.InnerText = $webUrl
+
+        [System.Xml.XmlNode] $sp_ListNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SharePointConnection/List")
+        $sp_ListNode.InnerText = $listTitle
+
+        [System.Xml.XmlNode] $userMapping_DefaultUserNameNode = $jobXmlDoc.SelectSingleNode("/TransferJob/JobOptions/UserMappingOptions/DefaultUserName")
+        $userMapping_DefaultUserNameNode.InnerText = $defaultUserName
+
+
+        [System.Xml.XmlNode] $sharepointDefNode = $jobXmlDoc.SelectSingleNode("/TransferJob/SharePointTargetDefinition")
+        
+    }
+    else
+    {
+        Write-Host "error in server name" -ForegroundColor Red
+    }
+
+
+
+
+    $jobXmlDoc.Save($NewJobFileFullPath)
+    ################################################################################
+}
+catch
+{
+    throw
+}
+finally
+{
+    [System.GC]::Collect()
+    [System.GC]::WaitForPendingFinalizers()
+}
+
+Write-Host ""
+Write-Host "Done!"
+
+################################################################################################################################################################
---
+++ PS_NMSP_EnsureList.ps1
@@ -0,0 +1,229 @@
+﻿
+################################################################################################################################################################
+# NMSP Ensure SPList
+################################################################################################################################################################
+Import-Module "J:\Arun\Git\DevEx.References\NuGet\microsoft.sharepointonline.csom.16.1.7414.1200\lib\net45\Microsoft.SharePoint.Client.dll"
+Import-Module "J:\Arun\Git\DevEx.References\NuGet\microsoft.sharepointonline.csom.16.1.7414.1200\lib\net45\Microsoft.SharePoint.Client.Runtime.dll"
+################################################################################################################################################################
+
+[string] $Username = "B13501@evonik.com"
+[SecureString] $Password = Read-Host "Enter Password for $($Username)" -AsSecureString
+[string] $SiteUrl   = "https://evonik.sharepoint.com/sites/10426"
+[string] $ListTitle = "Info"
+
+[PSCustomObject[]] $SharePointFieldSchemaTemplates = New-Object PSCustomObject[] 10
+
+################################################################################################################################################################
+# Functions
+################################################################################################################################################################
+
+Function Init-FieldSchemaTemplates
+{
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Text)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Single line of text"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value ""
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Text' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' MaxLength='255' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Note)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Multiple lines of text"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Plain text"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Text' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' NumLines='6' RichText='FALSE' AppendOnly='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Note)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Multiple lines of text"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Rich text (Bold, italics, text alignment, hyperlinks)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Text' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' NumLines='6' RichText='TRUE' RichTextMode='Compatible' IsolateStyles='FALSE' AppendOnly='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Note)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Multiple lines of text"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Enhanced rich text (Rich text with pictures, tables, and hyperlinks)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Text' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' NumLines='6' RichText='TRUE' RichTextMode='FullHtml' IsolateStyles='TRUE' RestrictedMode='TRUE' AppendOnly='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Choice)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Choice (menu to choose from)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Drop-Down Menu"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Choice' Format='Dropdown' FillInChoice='FALSE' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE'><Default></Default><CHOICES><CHOICE></CHOICE></CHOICES></Field>"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Choice)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Choice (menu to choose from)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Radio Buttons"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Choice' Format='RadioButtons' FillInChoice='FALSE' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE'><Default></Default><CHOICES><CHOICE></CHOICE></CHOICES></Field>"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Choice)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Choice (menu to choose from)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Checkboxes (allow multiple selections)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='MultiChoice' FillInChoice='FALSE' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE'><Default></Default><CHOICES><CHOICE></CHOICE></CHOICES></Field>"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Integer)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Number (1, 1.0, 100)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Number of decimal places (Automatic)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Number' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Integer)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Number (1, 1.0, 100)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Number of decimal places"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Number' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' Decimals='' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Integer)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Number (1, 1.0, 100)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Min and Max"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Number' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' Min='' Max='' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Integer)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Number (1, 1.0, 100)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Number of decimal places | Min and Max"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Number' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' Decimals='' Min='' Max='' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Currency)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Currency ($, ¥, €)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Number of decimal places (Automatic)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Currency' LCID='' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Currency)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Currency ($, ¥, €)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Number of decimal places"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Currency' LCID='' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' Decimals='' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Currency)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Currency ($, ¥, €)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Min and Max"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Currency' LCID='' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' Min='' Max='' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Currency)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Currency ($, ¥, €)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Number of decimal places | Min and Max"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Currency' LCID='' Name='' DisplayName='' StaticName='' Percentage='FALSE' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' Decimals='' Min='' Max='' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::DateTime)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Date and Time"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "DateOnly | Display Format (Standard)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='DateTime' Name='' StaticName='' DisplayName='' Format='DateOnly' FriendlyDisplayFormat='Disabled' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::DateTime)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Date and Time"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "DateTime | Display Format (Standard)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='DateTime' Name='' StaticName='' DisplayName='' Format='DateTime' FriendlyDisplayFormat='Disabled' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::DateTime)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Date and Time"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "DateOnly | Display Format (Friendly)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='DateTime' Name='' StaticName='' DisplayName='' Format='DateOnly' FriendlyDisplayFormat='Relative' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::DateTime)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Date and Time"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "DateTime | Display Format (Friendly)"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='DateTime' Name='' StaticName='' DisplayName='' Format='DateTime' FriendlyDisplayFormat='Relative' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Lookup)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Lookup (information already on this site)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value ""
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Lookup' Name='' StaticName='' DisplayName='' List='{876fa85c-ecd5-4654-80ec-e0412ce96f07}' ShowField='Title' RelationshipDeleteBehavior='None' Required='FALSE' EnforceUniqueValues='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Lookup)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Lookup (information already on this site)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value "Allow multiple values"
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='LookupMulti' Name='' StaticName='' DisplayName='' List='{876fa85c-ecd5-4654-80ec-e0412ce96f07}' ShowField='Title' Mult='TRUE' RelationshipDeleteBehavior='None' Required='FALSE' EnforceUniqueValues='FALSE' />"
+
+    $fldSchm = New-Object PSObject
+    $fldSchm | Add-Member NoteProperty -Name "FieldType" -TypeName "Microsoft.SharePoint.Client.FieldType" -Value ([Microsoft.SharePoint.Client.FieldType]::Boolean)
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeDescription" -Value "Yes/No (check box)"
+    $fldSchm | Add-Member NoteProperty -Name "FieldTypeOptions" -Value ""
+    $fldSchm | Add-Member NoteProperty -Name "SchemaXml" -Value "<Field Type='Boolean' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE'><Default>1</Default></Field>"
+
+<#
+
+
+<Field Type='User' List='UserInfo' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' UserSelectionMode='PeopleOnly' UserSelectionScope='0' />
+<Field Type='User' List='UserInfo' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' UserSelectionMode='PeopleAndGroups' UserSelectionScope='0' Group='' Mult='FALSE' />
+<Field Type='UserMulti' List='UserInfo' Name='' StaticName='' DisplayName='' Required='FALSE' EnforceUniqueValues='FALSE' UserSelectionMode='PeopleOnly' UserSelectionScope='3' Group='' Mult='TRUE' />
+
+
+
+#>
+}
+
+################################################################################################################################################################
+# Main Program
+################################################################################################################################################################
+
+try
+{
+    $context = New-Object Microsoft.SharePoint.Client.ClientContext($SiteUrl)
+    $credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username , $Password)
+    $context.Credentials = $credentials
+
+    Write-Host "Querying web/list data..."
+
+    $web = $context.Web
+    $context.Load($web)
+    $lists = $context.Web.Lists
+    $context.Load($lists)
+    $context.ExecuteQuery()
+
+    $list = $lists | Where { $_.Title -eq $ListTitle }
+
+    $fieldCollection = $null
+
+    if ($list)
+    {
+        Write-Host "$($ListTitle) - Exists"
+        $list = $context.Web.Lists.GetByTitle($ListTitle)
+        $fieldCollection = $list.Fields
+        $context.Load($fieldCollection)
+    }
+    else
+    {
+        Write-Host "$($ListTitle) - Creating..."
+        $listInfo = New-Object Microsoft.SharePoint.Client.ListCreationInformation
+        $listInfo.QuickLaunchOption = [Microsoft.SharePoint.Client.QuickLaunchOptions]::On
+        $listInfo.Title = $ListTitle
+        $listInfo.TemplateType = [int] [Microsoft.SharePoint.Client.ListTemplateType]::GenericList
+        $list = $context.Web.Lists.Add($listInfo)
+        $fieldCollection = $list.Fields
+        $context.Load($fieldCollection)
+    }
+
+    $context.Load($list)
+    $context.ExecuteQuery()
+
+
+
+
+
+}
+catch
+{
+    #Write-Host    $_.Exception.ToString() -ForegroundColor Red
+    throw
+}
+finally
+{
+    [System.GC]::Collect()
+    [System.GC]::WaitForPendingFinalizers()
+}
+
+Write-Host ""
+Write-Host "Done!"
+
+################################################################################################################################################################
