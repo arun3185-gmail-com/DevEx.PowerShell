902707517504d12357818f8e35304d8d0a68ebf9
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2018-11-26 23:57:12 +0530

    Add files via upload

---
+++ PS_MNSP_Debug1.ps1
@@ -0,0 +1,23 @@
+﻿
+################################################################################################################################################################
+# Quest - Migrator for Notes to SharePoint
+################################################################################################################################################################
+Import-Module "C:\Program Files (x86)\Quest\Migrator for Notes to SharePoint\Bin\Quest.NSP.SharePoint.Common.dll"
+Import-Module "C:\Program Files (x86)\Quest\Migrator for Notes to SharePoint\Bin\Quest.NSP.Transform.Common.dll"
+Import-Module "C:\Program Files (x86)\Quest\Migrator for Notes to SharePoint\Bin\Quest.NSP.Migrator.Common.dll"
+################################################################################################################################################################
+
+
+
+[Quest.NSP.Transform.QueryFactory] $qryFactory = [Quest.NSP.Transform.QueryFactory]::LoadFactory([Quest.NSP.Transform.DataSourceType]::Notes)
+[Quest.NSP.Migrator.Options] $opts = [Quest.NSP.Migrator.Options]::Load()
+
+[Quest.NSP.SharePoint.TransferJob] $transJob = New-Object Quest.NSP.SharePoint.TransferJob($qryFactory, [Quest.NSP.SharePoint.TargetType]::SharePoint)
+$transJob.JobOptions.PreserveDates = $true
+
+$opts.UserOptions.InitializeJobWithUserDefaults($transJob)
+
+
+[Quest.NSP.Transform.QuerySource] $qrySrc = $qryFactory.CreateQuerySource("DANLDA01/DA-DE/Server/Degussa","Rohmax/Supply Chain/RXGSCT.nsf")
+
+$qrySrc.MergeOptions($opts)
