ca11b2b1fa9d908e1fe197a7b1af939871cba787
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2018-11-30 00:00:28 +0530

    Add files via upload

---
+++ PS_FileToB64.ps1
@@ -0,0 +1,14 @@
+﻿
+$srcFilePath = ""
+
+[byte[]] $fileAsBytes = [System.IO.File]::ReadAllBytes($srcFilePath)
+[string] $strBase64 = [System.Convert]::ToBase64String($fileAsBytes)
+
+Set-Content -Encoding Ascii -Path "$($srcFilePath.Remove($srcFilePath.LastIndexOf("."))).txt" -Value $strBase64
+
+
+$dstFilePath = ""
+
+[string] $strBase64 = Get-Content -Encoding Ascii -Path $dstFilePath
+[byte[]] $fileAsBytes = [System.Convert]::FromBase64String($strBase64)
+[System.IO.File]::WriteAllBytes("$($dstFilePath.Remove($dstFilePath.LastIndexOf("."))).zip", $fileAsBytes)
