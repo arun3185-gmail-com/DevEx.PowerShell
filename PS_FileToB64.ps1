d9e0f72d4fa471bc7622081d8e7a0b4a01db7898
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2018-12-01 00:19:20 +0530

    Update PS_FileToB64.ps1

--- PS_FileToB64.ps1
+++ PS_FileToB64.ps1
@@ -1,14 +1,23 @@
 
-$srcFilePath = ""
+################################################################################################################################################################
 
+$srcFolder = ""
+
+$srcFilePath = "$($srcFolder).zip"
+Compress-Archive -Path $srcFolder -DestinationPath $srcFilePath
 [byte[]] $fileAsBytes = [System.IO.File]::ReadAllBytes($srcFilePath)
 [string] $strBase64 = [System.Convert]::ToBase64String($fileAsBytes)
-
 Set-Content -Encoding Ascii -Path "$($srcFilePath.Remove($srcFilePath.LastIndexOf("."))).txt" -Value $strBase64
-
+Remove-Item -Path $srcFilePath
+################################################################################################################################################################
 
 $dstFilePath = ""
 
+Add-Type -AssemblyName System.IO.Compression.FileSystem
 [string] $strBase64 = Get-Content -Encoding Ascii -Path $dstFilePath
 [byte[]] $fileAsBytes = [System.Convert]::FromBase64String($strBase64)
 [System.IO.File]::WriteAllBytes("$($dstFilePath.Remove($dstFilePath.LastIndexOf("."))).zip", $fileAsBytes)
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$($dstFilePath.Remove($dstFilePath.LastIndexOf("."))).zip","$($dstFilePath.Remove($dstFilePath.LastIndexOf(".")).Remove($dstFilePath.LastIndexOf("\")))")
+Remove-Item -Path "$($dstFilePath.Remove($dstFilePath.LastIndexOf("."))).zip"
+################################################################################################################################################################
+
