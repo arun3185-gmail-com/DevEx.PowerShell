9ae5d0566e65b3a192f5f1850933158727081fea
arun3185-gmail-com <40928588+arun3185-gmail-com@users.noreply.github.com>
2019-01-08 23:59:36 +0530

    Update PS_ImageProcessor_Debug1.ps1

--- PS_ImageProcessor_Debug1.ps1
+++ PS_ImageProcessor_Debug1.ps1
@@ -1,10 +1,55 @@
-﻿
+
 ################################################################################################################################################################
 # ImageProcessor Debug 1
 ################################################################################################################################################################
 Import-Module "D:\Arun\Git\DevEx.References\NuGet\imageprocessor.2.6.2.25\lib\net452\ImageProcessor.dll"
 ################################################################################################################################################################
 
+[string] $srcFilePath = "C:\Users\arunkumar.b08\Pictures\VietnamStairs_EN-AU4320366505_1366x768.jpg"
+
+[byte[]] $srcFileBytes = [System.IO.File]::ReadAllBytes($srcFilePath)
+
+[ImageProcessor.Imaging.Formats.ISupportedImageFormat] $format = New-Object ImageProcessor.Imaging.Formats.JpegFormat
+$format.Quality = 70
+[System.Drawing.Size] $size = New-Object System.Drawing.Size(150, 0)
+
+[System.IO.MemoryStream] $inStream = $null
+[System.IO.MemoryStream] $outStream = $null
+[ImageProcessor.ImageFactory] $imgFactory = $null
+
+try
+{
+    $inStream = New-Object System.IO.MemoryStream($srcFileBytes)
+    $outStream = New-Object System.IO.MemoryStream
+    $imgFactory = New-Object ImageProcessor.ImageFactory($true)
+
+    $imgFactory.Load($inStream).Resize($size).Format($format).Save($outStream)
+
+    [bytes[]] $dstFileBytes = New-Object bytes[$($outStream.Length)]
+    
+    
+}
+catch
+{}
+finally
+{
+    if ($imgFactory -ne $null) { $imgFactory.Dispose(); $imgFactory = $null }
+    if ($outStream -ne $null) { $outStream.Dispose(); $outStream = $null }
+    if ($inStream -ne $null) { $inStream.Dispose(); $inStream = $null }
+}
+
+
+
+
+
+<#
+try
+{}
+catch
+{}
+finally
+{}
+#>
 
 ################################################################################################################################################################
 # Main Program
